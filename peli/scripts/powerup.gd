139b40e1c211747984d29d4d21dab3f1dcdcf62b
ejkallio <eliasjaakko13@gmail.com>
2025-02-14 14:01:23 +0200

    added basic collectible

---
+++ peli/scripts/powerup.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("Got powerup")
+	queue_free()
+	timer.start()
+
+
+#func _on_timer_timeout() -> void:
+	#queue_redraw()
