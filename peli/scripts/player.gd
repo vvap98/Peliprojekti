98a966c75b97e14078e9c7c6b84bec5ae2fba336
ejkallio <eliasjaakko13@gmail.com>
2025-02-14 15:05:47 +0200

    double jump powerup WIP

--- peli/scripts/player.gd
+++ peli/scripts/player.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
 var hp = 3
 var can_attack = true
+var jumps = 1
 #func handleHp() -> void:
 	
 @onready var sprite_2d: Sprite2D = $Sprite2D
@@ -17,11 +18,17 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jumps > 0:
 		velocity.y = JUMP_VELOCITY
+		jumps -= 1
+		print(jumps)
+	# Stop jump if key is released
 	if Input.is_action_just_released("jump") && velocity.y < 0:
 		velocity.y = 0
-
+	
+	if is_on_floor():
+		jumps = 1
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("moveLeft", "moveRight")
