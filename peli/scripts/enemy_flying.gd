d9c221fbadbfc475ce6177c58f7a2a6cdf2becdc
ejkallio <eliasjaakko13@gmail.com>
2025-03-19 13:10:25 +0200

    lentävälle vihulle lisätty navigaatio (BUGINEN)

--- peli/scripts/enemy_flying.gd
+++ peli/scripts/enemy_flying.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 # TODO vihun idlekäyttäytyminen
 
-const SPEED = 2.5
+const SPEED = 100
 var direction = 1
 var playerchase = false
 
@@ -11,28 +11,33 @@ var ogposition
 
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
 
 var player # = null
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
+	makePath()
 	ogposition = position
 	
 func _physics_process(delta: float) -> void:
 	#Get gravity
 	#if not is_on_floor():
 	#	velocity += get_gravity() * delta
-	if playerchase:
-		position += (player.position - position).normalized() * SPEED
-	else:
-		position += (ogposition - position).normalized() * SPEED
+	#if playerchase:
+	#var next_path_pos := nav_agent.get_next_path_position()
+	var dir = to_local(nav_agent.get_next_path_position()).normalized()
+	#print(player.global_position)
+	#print(dir)
+	velocity = dir * SPEED
+		#position += (player.position - position).normalized() * SPEED
+	#lif position != ogposition:
+	#	position += (ogposition - position).normalized() * SPEED
 		#if ray_cast_right.is_colliding(): # or player.position.x < position.x and player.position.x >= position.x - 200:
 		#	direction = -1
 		#if ray_cast_left.is_colliding(): #or player.position.x > position.x and player.position.x <= position.x + 200:
 		#	direction = 1
-	
-	
-		
 		#position.x += direction * SPEED * delta
+		
 	#print(position)
 	
 	move_and_slide()
@@ -47,6 +52,10 @@ func _on_detection_zone_body_entered(player) -> void:
 func _on_detection_zone_body_exited(player) -> void:
 	#player = null
 	playerchase = false
+	
+func makePath() -> void:
+	nav_agent.target_position = player.global_position 
+	print(nav_agent.target_position)
 
 func checkHp():
 	if hp <= 0:
@@ -61,3 +70,7 @@ func enemyDeath():
 	# self.visible = false
 	# self.set_deferred("monitoring", false)
 	queue_free()
+
+
+func _on_timer_timeout() -> void:
+	makePath()
