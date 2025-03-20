63d0424be6cb31813677fd346e8756a269d4686d
ejkallio <eliasjaakko13@gmail.com>
2025-03-20 12:14:58 +0200

    ampuvan vihun scene lisätty (KESKEN)

---
+++ peli/scripts/enemy_shooting.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var hp = 3
+
+func _process(_delta: float) -> void:
+	checkHp()
+
+func checkHp():
+	if hp <= 0:
+		enemyDeath()
+
+func getDamaged():
+	hp = hp - 1
+	print(hp)
+	
+func enemyDeath():
+	print("ded")
+	# self.visible = false
+	# self.set_deferred("monitoring", false)
+	queue_free()
