d72c6ebedc3721c513c1dd7b8dd9909242529631
ejkallio <eliasjaakko13@gmail.com>
2025-03-20 13:06:57 +0200

    Ampuva vihollinen ampuu

--- peli/scripts/enemy_shooting.gd
+++ peli/scripts/enemy_shooting.gd
@@ -1,9 +1,22 @@
 extends CharacterBody2D
 
 var hp = 3
+#@onready var world = get_tree().get_root().get_node("world")
+@onready var projectile = load("res://scenes/projectile.tscn")
+var world
 
 func _process(_delta: float) -> void:
 	checkHp()
+	
+func _ready() -> void:
+	world = get_tree().get_root().get_node("World")
+
+func shoot():
+	var instance = projectile.instantiate()
+	instance.dir = rotation
+	instance.spawnPos = global_position
+	instance.spawnRot = rotation
+	world.add_child.call_deferred(instance)
 
 func checkHp():
 	if hp <= 0:
@@ -18,3 +31,7 @@ func enemyDeath():
 	# self.visible = false
 	# self.set_deferred("monitoring", false)
 	queue_free()
+
+
+func _on_timer_timeout() -> void:
+	shoot()
