5db8c529b333f580d82fde2f93c8e9047e835e47
ejkallio <eliasjaakko13@gmail.com>
2025-03-20 12:39:26 +0200

    ampuvan vihollisen ammus lisätty

--- peli/scripts/killzone.gd
+++ peli/scripts/killzone.gd
@@ -6,7 +6,7 @@ var player
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
 	
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if overlaps_body(player):
 		#print("overlapping")
 		player.getDamaged()
