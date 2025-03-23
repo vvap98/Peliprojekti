1946a9aca7e3762ab2d5ea30af245b7e4845b0b3
vvap98 <veeti.puttonen@protonmail.com>
2025-03-23 06:24:00 +0200

    pausemenu toimii esc painamalla. fullscreen

---
+++ peli/pauseMenu.gd
@@ -0,0 +1,42 @@
+extends Control
+
+
+
+func _ready():
+	$AnimationPlayer.play("RESET")
+		
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("menuBlur")
+	visible = false
+func pause():
+	get_tree().paused = true
+	visible = true
+	$AnimationPlayer.play("menuBlur")
+	
+func escPressed():
+	if Input.is_action_just_pressed("esc"):
+		if get_tree().paused == false:
+			pause()
+		elif get_tree().paused == true:
+			resume()
+
+
+func _on_resume_pressed() -> void:
+	resume()
+
+func _on_restart_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _process(delta):
+	escPressed()
