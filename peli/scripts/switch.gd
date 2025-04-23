351baa75a3be0f497b4eed2ec99aa504c54bbb26
ejkallio <eliasjaakko13@gmail.com>
2025-04-23 12:51:16 +0300

    toimiva nappi ja ovi

--- peli/scripts/switch.gd
+++ peli/scripts/switch.gd
@@ -7,4 +7,5 @@ func _on_body_entered(body: Node2D) -> void:
 		print("gate disabled!")
 		gate.visible = false
 		gate.set_deferred("monitoring", false)
-	
+		gate.get_node("CollisionShape2D").set_deferred("disabled", true)
+		print(gate.get_node("CollisionShape2D"))
