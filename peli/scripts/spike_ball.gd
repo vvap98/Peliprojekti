d9a8abd07997ee0954f71a81df4f78e71c126954
ejkallio <eliasjaakko13@gmail.com>
2025-04-24 10:39:35 +0300

    piikkipallot katoavat hetken päästä koskiessaan maahan

--- peli/scripts/spike_ball.gd
+++ peli/scripts/spike_ball.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 var spawnPos : Vector2
+var air = true
+
+@onready var timer: Timer = $Timer
 
 func _ready() -> void:
 	global_position = spawnPos
@@ -9,5 +12,12 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	elif air == true:
+		air = false
+		timer.start()
+	
 	move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
