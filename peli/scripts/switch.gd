3a79ac3e62164774da833f6138db1ae55ceb4c1d
ejkallio <eliasjaakko13@gmail.com>
2025-04-25 12:39:50 +0300

    nappi korjattu

--- peli/scripts/switch.gd
+++ peli/scripts/switch.gd
@@ -3,9 +3,10 @@ extends Area2D
 @export var gate : unlockableGate
 
 func _on_body_entered(body: Node2D) -> void:
+	self.scale = Vector2(1, 0.5)
+	
 	if gate and gate.buttons != 0:
 		gate.buttons = gate.buttons - 1
 		print("gate has ", gate.buttons, "buttons left!")
 		if gate.buttons == 0:
-			self.scale = Vector2(1, 0.5)
 			self.get_node("CollisionShape2D").set_deferred("disabled", true)
