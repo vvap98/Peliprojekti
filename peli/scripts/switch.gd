e14a3ef839b02394db4ab3c918211deb0679dc80
ejkallio <eliasjaakko13@gmail.com>
2025-04-25 19:13:28 +0300

    nappibugi korjattu

--- peli/scripts/switch.gd
+++ peli/scripts/switch.gd
@@ -4,9 +4,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	self.scale = Vector2(1, 0.5)
-	
+	self.get_node("CollisionShape2D").set_deferred("disabled", true)
 	if gate and gate.buttons != 0:
 		gate.buttons = gate.buttons - 1
 		print("gate has ", gate.buttons, "buttons left!")
-		if gate.buttons == 0:
-			self.get_node("CollisionShape2D").set_deferred("disabled", true)
