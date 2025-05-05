ef4e5c519586e4a6578c3bcd6509588b1674f9f7
ejkallio <eliasjaakko13@gmail.com>
2025-05-05 15:10:09 +0300

    napeille lisätty spritet

--- peli/scripts/holdable_switch.gd
+++ peli/scripts/holdable_switch.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 @export var gate : unlockableGate
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _on_body_entered(body: Node2D) -> void:
-	self.scale = Vector2(1, 0.5)
+	#self.scale = Vector2(1, 0.5)
+	sprite_2d.frame = 3
 	if gate and gate.buttons != 0:
 		gate.buttons = gate.buttons - 1
 		print("gate has ", gate.buttons, "buttons left!")
@@ -15,5 +17,6 @@ func _on_body_exited(body: Node2D) -> void:
 	if gate and gate.buttons == 0:
 		gate.buttons = gate.buttons + 1
 		if gate.buttons != 0:
-			self.scale = Vector2(1, 1)
+			#self.scale = Vector2(1, 1)
+			sprite_2d.frame = 1
 			#self 
--- peli/scripts/switch.gd
+++ peli/scripts/switch.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
 @export var gate : unlockableGate
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _on_body_entered(body: Node2D) -> void:
-	self.scale = Vector2(1, 0.5)
+	#self.scale = Vector2(1, 0.5)
 	self.get_node("CollisionShape2D").set_deferred("disabled", true)
+	sprite_2d.frame = 2
 	if gate and gate.buttons != 0:
 		gate.buttons = gate.buttons - 1
 		print("gate has ", gate.buttons, "buttons left!")
