517d859be53092991be897dd284204d8fdcf305a
ejkallio <eliasjaakko13@gmail.com>
2025-05-05 14:52:11 +0300

    piikkiansat voi nyt kääntää + muutamia muokkauksia kenttään

--- peli/scripts/spike_trap.gd
+++ peli/scripts/spike_trap.gd
@@ -5,8 +5,15 @@ var world
 @onready var spikeball = load("res://scenes/spike_ball.tscn")
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
+@export var on_wall : bool
+@export var flipped : bool 
+
 func _ready() -> void:
 	world = get_tree().get_root().get_node("World")
+	if on_wall:
+		self.rotation_degrees = 90
+	if flipped:
+		self.rotation_degrees += 180
 
 #func _on_body_entered(body: Node2D) -> void:
 	#if body.is_in_group("player"):
