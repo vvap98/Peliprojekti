3b06fa92a98401db807de5501ebcce52465d9f6b
ejkallio <eliasjaakko13@gmail.com>
2025-05-16 15:58:10 +0300

    timerien aikaa voi säätää editorista

--- peli/scripts/text_collectable.gd
+++ peli/scripts/text_collectable.gd
@@ -1,15 +1,16 @@
 extends Area2D
 
 @export var label : Label
+@export var timer_amount : float = 15.0
 @onready var timer: Timer = $Timer
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-
 var player
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
-
+	timer.wait_time = timer_amount
+	
 func _on_body_entered(body: Node2D) -> void:
 	if body == player and label:
 		label.visible = true
