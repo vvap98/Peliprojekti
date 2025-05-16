c5315b847448229f7dbf4bd758d4a967bfd677da
ejkallio <eliasjaakko13@gmail.com>
2025-05-16 14:34:02 +0300

    tarinacollectiblet lisätty

---
+++ peli/scripts/text_collectable.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+@export var label : Label
+@onready var timer: Timer = $Timer
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+var player
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body == player and label:
+		label.visible = true
+		timer.start()
+		sprite_2d.visible = false
+		collision_shape_2d.set_deferred("disabled", true)
+
+func _on_timer_timeout() -> void:
+	if label:
+		label.visible = false
+		sprite_2d.visible = true
+		collision_shape_2d.set_deferred("disabled", false)
