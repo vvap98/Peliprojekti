49b24e2ce5d57450b6762966d3b2b7f21e72b383
ejkallio <eliasjaakko13@gmail.com>
2025-05-17 14:06:15 +0300

    poweruptekstit

--- peli/scripts/dash_power_up.gd
+++ peli/scripts/dash_power_up.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 @onready var label: Label = $Label
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 var player
 func _ready():
@@ -11,12 +13,12 @@ func _on_body_entered(body: Node2D) -> void:
 	if body == player:
 		print("Got Dash!")
 		label.visible = true
-		#timer.start()
+		timer.start()
 		player.can_dash = true
 		player.dash_icon.visible = true
-		queue_free()
-		#self.visible = false
-		#self.set_deferred("monitoring", false)
+		#queue_free()
+		sprite_2d.visible = false
+		collision_shape_2d.disabled = true
 
 
 func _on_timer_timeout() -> void:
--- peli/scripts/powerup.gd
+++ peli/scripts/powerup.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 @onready var label: Label = $Label
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 var player
 func _ready():
@@ -11,12 +13,12 @@ func _on_body_entered(body: Node2D) -> void:
 	if body == player:
 		print("Got Double Jump!")
 		label.visible = true
-		#timer.start()
+		timer.start()
 		player.can_double_jump = true
 		player.double_jump_icon.visible = true
-		queue_free()
-		#self.visible = false
-		#self.set_deferred("monitoring", false)
+		#queue_free()
+		sprite_2d.visible = false
+		collision_shape_2d.disabled = true
 
 
 func _on_timer_timeout() -> void:
